3aa5a22744f58dd4997a4796b58de4dab23864de
ledob44 <olivier.imbert@unit.eu>
2025-12-02 11:12:00 +0100

    boutons liens ajoutés dans les colonnes

--- src/render.php
+++ src/render.php
@@ -10,22 +10,34 @@ $centresEtab = new WP_Query([
 	'post_type' => 'centre-formation',
 	'posts_per_page' => -1,
 	'meta_query' => [
+		'relation' => 'AND',
 		[
 			'key' => 'type_de_centre',
 			'value' => 'Etablissement scolaire partenaire',
 			'compare' => '=',
 		],
+		[
+			'key' => 'latitude',
+			'value' => '',
+			'compare' => '!='
+		]
 	]
 ]);
 $centresPros = new WP_Query([
 	'post_type' => 'centre-formation',
 	'posts_per_page' => -1,
 	'meta_query' => [
+		'relation' => 'AND',
 		[
 			'key' => 'type_de_centre',
 			'value' => 'Centre de formation constructeurs',
 			'compare' => '=',
 		],
+		[
+			'key' => 'latitude',
+			'value' => '',
+			'compare' => '!='
+		]
 	]
 ]);
 
@@ -44,21 +56,22 @@ if (count($centres) > 0) :
 		$image = $photo ? $photo['sizes']['thumbnail'] : '';
 
 		$desc = $image . '<b>' . $name . '</b><br>' . $typeCentre . '<br><br>' . $adresse;
-
-		$coords_array[] = [
-			"name" => $name,
-			"adresse" => nl2br($adresse),
-			"latitude" => $latitude,
-			"longitude" => $longitude,
-			"marker" => $typeCentre === 'Centre de formation constructeurs' ? 1 : 2,
-			"description" => $desc,
-			"typeCentre" => $typeCentre,
-			"centreFormation" => $centre->ID,
-			"image" => $image,
-			"id" => $i,
-			"catalog" => get_permalink(CATALOG_PAGE),
-		];
-		$i++;
+		if(!empty($latitude) || !empty($longitude)) {
+			$coords_array[] = [
+				"name" => $name,
+				"adresse" => nl2br($adresse),
+				"latitude" => $latitude,
+				"longitude" => $longitude,
+				"marker" => $typeCentre === 'Centre de formation constructeurs' ? 1 : 2,
+				"description" => $desc,
+				"typeCentre" => $typeCentre,
+				"centreFormation" => $centre->ID,
+				"image" => $image,
+				"id" => $i,
+				"catalog" => get_permalink(CATALOG_PAGE),
+			];
+			$i++;
+		}
 	endforeach;
 
 	$data_to_pass = [
@@ -78,6 +91,9 @@ if (count($centres) > 0) :
 			<h3 class="mb-4">Nos centres de formation</h3>
 			<div class="d-flex flex-md-row flex-column">
 				<div class="col-md-6 pe-md-3">
+					<a href="<?php echo get_permalink(CATALOG_PAGE); ?>?type_de_formation=base" class="btn btn-base">
+						Formations des établissements
+					</a>
 					<h4>Établissements scolaires partenaires</h4>
 					<ul>
 						<?php
@@ -91,6 +107,9 @@ if (count($centres) > 0) :
 					</ul>
 				</div>
 				<div class="col-md-6 ps-md-3">
+					<a href="<?php echo get_permalink(CATALOG_PAGE); ?>?type_de_formation=avance" class="btn btn-avance">
+						Formations constructeurs
+					</a>
 					<h4>Centres de formations constructeurs</h4>
 					<ul>
 						<?php
